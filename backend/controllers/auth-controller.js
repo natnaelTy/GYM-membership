eba5306c9819c132e9dc26c46b1d85379d0ca76b
natnael taye <natitaye316@gmail.com>
2025-03-08 17:39:40 +0300

    added login and logout routes

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -3,8 +3,11 @@ import bcrypt from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
 
 
-export const signup = async (req, res) => {
-  const { full_name, phone_number, gender, user_password, image } = req.body;
+// register the user
+export const register =  async (req, res) => {
+  const { full_name, phone_number, gender, user_password } = req.body;
+  const image = req.file ? req.file.path : null;
+
   try {
     if (!full_name || !phone_number || !user_password ||  !gender || !image) {
       return res
@@ -25,10 +28,10 @@ export const signup = async (req, res) => {
       if (result.length === 0) {
          // hash password
          const hashedPassword = await bcrypt.hash(user_password, 10);
-        // insert new user to the database
+        // insert new user in to the database
         const query = 'INSERT INTO users ( full_name, phone_number, user_password, gender, image ) VALUES ( ?, ?, ?, ?, ? )';
 
-        pool.query(query, [full_name, phone_number, hashedPassword, gender, image], (error, result) => {
+        pool.query(query, [full_name, phone_number, hashedPassword, gender, image], async (error, result) => {
           if (error){
             res.status(500).json({success: false, message: "Database error"});
           }
@@ -52,3 +55,59 @@ export const signup = async (req, res) => {
     console.log(err);
   }
 };
+
+// login
+
+export const login = (req, res) => {
+
+  const { phone_number, user_password } = req.body;
+
+  try{
+    if(!phone_number || !user_password){
+      return res.json({success: false, message: "Invalid Phone number or Password"});
+    }
+    const loginQuery = "SELECT * FROM users WHERE phone_number = ? ";
+
+    pool.query(loginQuery, [phone_number], async (error, result) => {
+      if(error){
+        console.log(`Error occured in the database ${error}`);
+        return res.status(500).send("Server Error");
+      }
+      
+    const user = await result[0];
+    // check if phone number is valid 
+    if(result.length === 0){
+      return res.status(400).json({success: false, message: "Incorrect phone number or password!"});
+    }
+    // check if user password is valid
+    const isPasswordValid = await bcrypt.compare(user_password, user.user_password);
+    if(!isPasswordValid){
+      return res.status(400).json({success: false, message: "Incorrect phone number or password!"});
+    }
+    // return new token to the user
+    const token = generateTokenAndSetCookie(res, user.userId, user.full_name, phone_number);
+    // if this all valid login and return the user
+    return res.status(200).json({success: true, message: "You are successfully logged in", user: {...user, user_password: undefined}, token });
+    });
+  }catch(err){
+    console.log(`Error ${err}`);
+    res.status(500).send("Server Error");
+  }
+}
+
+// logout
+
+export const logout = async (_, res) => {
+  res.clearCookie("token");
+  res.status(200).json({success: true, message: "You are successfully logged out"});
+
+  // delete the user from database
+  const deleteQuery = "DELETE FROM users WHERE phone_number = 0975348825 ";
+  pool.query(deleteQuery, (error, result) => {
+    if(error){
+      console.log(`Error occured in database while deleting user ${error}`);
+      return res.status(500).send("Server Error");
+    }
+  
+  })
+}
